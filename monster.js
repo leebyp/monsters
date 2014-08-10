fdb7240368c213b33ca6dcde77f3f506eac7ebef
Benjamin Lee <leebyp@gmail.com>
2014-08-10 04:28:45 +0100

    Refactored logic

--- monster.js
+++ monster.js
@@ -3,78 +3,97 @@
 
 var fs = require('fs');
 
-var monsterCount = process.argv[2];
-
 var randomValue = function (array) {
   return array[Math.floor(Math.random() * array.length)];
 };
 
-// synchronously read contents of map.txt to build locations object with nearby information
-var mapString = fs.readFileSync('map.txt', 'utf8');
-var locationsArray = mapString.split('\n');
-locationsArray.pop();
-var locations = {};
-for (var i=0; i<locationsArray.length; i++) {
-  var locationLinks = locationsArray[i].split(/[ =]/);
-  var individualLocation = locations[locationLinks[0]] = {};
-  for (var j=1; j<locationLinks.length; j+=2) {
-    individualLocation[locationLinks[j]] = locationLinks[j+1];
-  }
-}
-
-// create monsters at random locations with temporary names
-var monsters = {};
-for (var i=1; i<=monsterCount; i++) {
-  monsters[i] = {};
-  var locationNames = Object.keys(locations);
-  monsters[i].location = randomValue(locationNames);
-}
-
-// move each monster
-for (var monster in monsters) {
-  var oldLocation = monsters[monster].location;
-  // find random path and update monster's location
-  var directions = Object.keys(locations[oldLocation]);
-  var randomDirection = randomValue(directions);
-  if (randomDirection) {
-    monsters[monster].location = locations[oldLocation][randomDirection];
-  } else {
-    monsters[monster].location = oldLocation;
-  }
-}
-
-// check if any monsters are at the same location
-var tempLocations = {};
-for (var monster in monsters) {
-  var tempLocation = monsters[monster].location;
-  var tempMonsters = tempLocations[tempLocation];
-  if (tempMonsters) {
-    tempMonsters.push(monster);
-  } else {
-    tempMonsters = [monster];
-  }
-}
-// destroy relevant monsters, cities and paths
-for (tempLocation in tempLocations) {
-  var tempMonsters = tempLocations[tempLocation];
-  if (tempMonsters.length > 1) {
-    var logger = tempLocation + ' has been destroyed by ';
-    for (var i=0; i<tempMonsters.length; i++) {
-      delete monsters[tempMonsters[i]];
-      if (i !== tempMonsters.length-1) {
-        logger += 'monster ' + tempMonsters[i] + ', ';
-      } else {
-        logger += 'and monster ' + tempMonsters[i];
-      }
+var createLocations = function (mapFile) {
+  var mapString = fs.readFileSync(mapFile, 'utf8');
+  var locationsArray = mapString.split('\n');
+  locationsArray.pop();
+  var locations = {};
+  for (var i=0; i<locationsArray.length; i++) {
+    var locationLinks = locationsArray[i].split(/[ =]/);
+    var individualLocation = locations[locationLinks[0]] = {};
+    for (var j=1; j<locationLinks.length; j+=2) {
+      individualLocation[locationLinks[j]] = locationLinks[j+1];
     }
-    for (var tempLocationCheck in locations) {
-      for (var direction in locations[tempLocationCheck]) {
-        if (tempLocation === locations[tempLocationCheck][direction]) {
-          delete locations[tempLocationCheck][direction];
+  }
+  return locations;
+};
+
+var createMonsters = function (monstersCount, locations) {
+  var monsters = {};
+  for (var i=1; i<=monstersCount; i++) {
+    monsters[i] = {};
+    var locationNames = Object.keys(locations);
+    monsters[i].location = randomValue(locationNames);
+  }
+  return monsters;
+};
+
+var moveMonsters = function (monsters, locations) {
+  for (var monster in monsters) {
+    var oldLocation = monsters[monster].location;
+    // find random path and update monster's location
+    var directions = Object.keys(locations[oldLocation]);
+    var randomDirection = randomValue(directions);
+    if (randomDirection) {
+      monsters[monster].location = locations[oldLocation][randomDirection];
+    } else {
+      monsters[monster].location = oldLocation;
+    }
+  }
+};
+
+var fightMonsters = function (monsters, locations) {
+  // check if any monsters are at the same location
+  var tempLocations = {};
+  for (var monster in monsters) {
+    var tempLocation = monsters[monster].location;
+    var tempMonsters = tempLocations[tempLocation];
+    if (tempMonsters) {
+      tempMonsters.push(monster);
+    } else {
+      tempMonsters = [monster];
+    }
+  }
+  // destroy relevant monsters, cities and paths
+  for (var tempLocation2 in tempLocations) {
+    var tempMonsters2 = tempLocations[tempLocation2];
+    if (tempMonsters2.length > 1) {
+      var logger = tempLocation2 + ' has been destroyed by ';
+      for (var i=0; i<tempMonsters2.length; i++) {
+        delete monsters[tempMonsters2[i]];
+        if (i !== tempMonsters2.length-1) {
+          logger += 'monster ' + tempMonsters2[i] + ', ';
+        } else {
+          logger += 'and monster ' + tempMonsters2[i];
         }
       }
+      for (var tempLocationCheck in locations) {
+        for (var direction in locations[tempLocationCheck]) {
+          if (tempLocation2 === locations[tempLocationCheck][direction]) {
+            delete locations[tempLocationCheck][direction];
+          }
+        }
+      }
+      delete locations[tempLocation2];
+      console.log(logger);
     }
-    delete locations[tempLocation];
-    console.log(logger);
   }
-}
+};
+
+var monstersCount = process.argv[2];
+
+// synchronously read contents of map.txt to build locations object with nearby information
+var locations = createLocations('map.txt');
+
+// create monsters at random locations with temporary names
+var monsters = createMonsters(monstersCount, locations);
+
+// move each monster
+moveMonsters(monsters, locations);
+
+// fight monsters at same location
+fightMonsters(monsters, locations);
